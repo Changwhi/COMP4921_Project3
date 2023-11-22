3eda0189c55f5a70bf7393bd4a26ab0f4f903c5c
Felix <flieu@my.bcit.ca>
2023-11-22 14:45:17 -0800

    feat: resolved issue with sign up page button displaying multiple buttons

--- routes/router.js
+++ routes/router.js
@@ -81,148 +81,148 @@ router.get("/week", async (req,res) => {
 })
 
 
-// router.get("/login", async (req, res) => {
-//   const isLoggedIn = isValidSession(req)
-//   res.render("login", {
-//     isLoggedIn: isLoggedIn,
-//     message: null
-//   });
-//   return;
-// });
+router.get("/login", async (req, res) => {
+  const isLoggedIn = isValidSession(req)
+  res.render("login", {
+    isLoggedIn: isLoggedIn,
+    message: null
+  });
+  return;
+});
 
-// router.get('/logout', (req, res) => {
-//   console.log("Logging out");
+router.get('/logout', (req, res) => {
+  console.log("Logging out");
 
-//   req.session.destroy(err => {
-//     if (err) {
-//       console.error('Error destroying session:', err);
-//       return res.status(500).send('Failed to log out');
-//     }
+  req.session.destroy(err => {
+    if (err) {
+      console.error('Error destroying session:', err);
+      return res.status(500).send('Failed to log out');
+    }
 
-//     res.redirect('/login');
-//   });
-// });
+    res.redirect('/login');
+  });
+});
 
-// router.get("/signup", async (req, res) => {
-//   console.log("checking" + req.query.invalid)
-//   var invalid = req.query.invalid === undefined ? true : req.query.invalid;
-//   res.render("signup", {
-//     invalid: invalid,
-//     isLoggedIn: false
-//   });
-//   return;
+router.get("/signup", async (req, res) => {
+  console.log("checking" + req.query.invalid)
+  var invalid = req.query.invalid === undefined ? true : req.query.invalid;
+  res.render("signup", {
+    invalid: invalid,
+    isLoggedIn: false
+  });
+  return;
 
-// });
+});
 
-// router.post("/loggingin", async (req, res) => {
-//   var email = req.body.email;
-//   var password = req.body.password;
-//   var users = await db_users.getUsers();
-//   let user;
+router.post("/loggingin", async (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  var users = await db_users.getUsers();
+  let user;
 
-//   for (let i = 0; i < users.length; i++) {
-//     if (users[i].email == email) {
-//       user = users[i];
-//       break;
-//     }
-//   }
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email == email) {
+      user = users[i];
+      break;
+    }
+  }
 
-//   if (user === undefined) {
-//     res.render('login', {
-//       message: "Why did you enter the wrong email?!",
-//       isLoggedIn: false
-//     });
-//     return;
-//   }
+  if (user === undefined) {
+    res.render('login', {
+      message: "Why did you enter the wrong email?!",
+      isLoggedIn: false
+    });
+    return;
+  }
 
-//   const validationResult = passwordSchema.validate({
-//     password: password
-//   });
-//   if (validationResult.error) {
-//     let errorMsg = validationResult.error.details[0].message;
+  const validationResult = passwordSchema.validate({
+    password: password
+  });
+  if (validationResult.error) {
+    let errorMsg = validationResult.error.details[0].message;
 
-//     if (errorMsg.includes("(?=.*[a-z])")) {
-//       errorMsg = "Password must have at least 1 lowercase.";
-//     } else if (errorMsg.includes("(?=.*[A-Z])")) {
-//       errorMsg = "Password must have at least 1 uppercase.";
-//     } else if (errorMsg.includes("(?=.*[!@#$%^&*])")) {
-//       errorMsg = "Password requires 1 special character.";
-//     } else if (errorMsg.includes("(?=.*[0-9])")) {
-//       errorMsg = "Password needs to have 1 number.";
-//     } else {
-//       errorMsg = null;
-//     }
+    if (errorMsg.includes("(?=.*[a-z])")) {
+      errorMsg = "Password must have at least 1 lowercase.";
+    } else if (errorMsg.includes("(?=.*[A-Z])")) {
+      errorMsg = "Password must have at least 1 uppercase.";
+    } else if (errorMsg.includes("(?=.*[!@#$%^&*])")) {
+      errorMsg = "Password requires 1 special character.";
+    } else if (errorMsg.includes("(?=.*[0-9])")) {
+      errorMsg = "Password needs to have 1 number.";
+    } else {
+      errorMsg = null;
+    }
 
-//     res.render("error", {
-//       message: errorMsg,
-//       isLoggedIn: false
-//     });
-//     return;
-//   }
+    res.render("error", {
+      message: errorMsg,
+      isLoggedIn: false
+    });
+    return;
+  }
 
-//   const isValidPassword = bcrypt.compareSync(password, user.hashed_password);
-//   if (isValidPassword) {
-//     console.log("User's logged in")
-//     req.session.userID = user.user_id;
-//     req.session.name = user.name
-//     req.session.authenticated = true;
-//     req.session.email = email;
-//     req.session.cookie.maxAge = expireTime;
-//     res.redirect("/")
-//   } else {
-//     req.session.authenticated = false;
-//     res.redirect('/login');
-//   }
-// });
+  const isValidPassword = bcrypt.compareSync(password, user.hashed_password);
+  if (isValidPassword) {
+    console.log("User's logged in")
+    req.session.userID = user.user_id;
+    req.session.name = user.name
+    req.session.authenticated = true;
+    req.session.email = email;
+    req.session.cookie.maxAge = expireTime;
+    res.redirect("/")
+  } else {
+    req.session.authenticated = false;
+    res.redirect('/login');
+  }
+});
 
 
-// // // User creation
-// router.post("/submitUser", async (req, res) => {
-//   var email = req.body.email;
-//   var password = req.body.password;
-//   var name = req.body.name;
-//   var hashedPassword = bcrypt.hashSync(password, saltRounds);
+ // User creation
+router.post("/submitUser", async (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  var name = req.body.name;
+  var hashedPassword = bcrypt.hashSync(password, saltRounds);
 
-//   const validationResult = passwordSchema.validate({
-//     password
-//   });
+  const validationResult = passwordSchema.validate({
+    password
+  });
 
-//   if (validationResult.error) {
-//     let errorMsg = validationResult.error.details[0].message;
+  if (validationResult.error) {
+    let errorMsg = validationResult.error.details[0].message;
 
-//     if (errorMsg.includes("(?=.*[a-z])")) {
-//       errorMsg = "Password must have at least 1 lowercase.";
-//     } else if (errorMsg.includes("(?=.*[A-Z])")) {
-//       errorMsg = "Password must have at least 1 uppercase.";
-//     } else if (errorMsg.includes("(?=[!@#$%^&*])")) {
-//       errorMsg = "Password requires 1 special character.";
-//     } else if (errorMsg.includes("(?=.*[0-9])")) {
-//       errorMsg = "Password needs to have 1 number.";
-//     }
-//     res.render("signup", {
-//       message: errorMsg,
-//       isLoggedIn: false
-//     });
-//     return;
-//   } else {
-//     var success = await db_users.createUser({
-//       email: email,
-//       hashedPassword: hashedPassword,
-//       name: name
-//     });
+    if (errorMsg.includes("(?=.*[a-z])")) {
+      errorMsg = "Password must have at least 1 lowercase.";
+    } else if (errorMsg.includes("(?=.*[A-Z])")) {
+      errorMsg = "Password must have at least 1 uppercase.";
+    } else if (errorMsg.includes("(?=[!@#$%^&*])")) {
+      errorMsg = "Password requires 1 special character.";
+    } else if (errorMsg.includes("(?=.*[0-9])")) {
+      errorMsg = "Password needs to have 1 number.";
+    }
+    res.render("signup", {
+      message: errorMsg,
+      isLoggedIn: false
+    });
+    return;
+  } else {
+    var success = await db_users.createUser({
+      email: email,
+      hashedPassword: hashedPassword,
+      name: name
+    });
 
-//     if (success) {
-//       res.render("index", {
-//         isLoggedIn: true
-//       })
-//     } else {
-//       res.render('error', {
-//         message: `Failed to create the user ${email}, ${name}`,
-//         title: "User creation failed"
-//       });
-//     }
-//   }
-// });
+    if (success) {
+      res.render("index", {
+        isLoggedIn: true
+      })
+    } else {
+      res.render('error', {
+        message: `Failed to create the user ${email}, ${name}`,
+        title: "User creation failed"
+      });
+    }
+  }
+});
 
 
 module.exports = router;
